9ce9d50a62bde6abaac791edb5e177553d206556
Pat LaVarre <p.lavarre@ieee.org>
2022-11-10 12:42:02 -0800

    byoverbs/bin/: Add :h :v to max out Sh Terminal window horizontal width/ vertical height

---
+++ bin/git.py
@@ -0,0 +1,14 @@
+_ = """
+
+$ git clean --help
+
+       -e <pattern>, --exclude=<pattern>
+           Use the given exclude pattern in addition to the standard ignore
+           rules (see gitignore(5)).
+
+       -n, --dry-run
+           Don't actually remove anything, just show what would be done.
+
+$ git clean -fdx -e /env
+
+"""
