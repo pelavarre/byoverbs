d7207d6012363d3a35ebd90f27c28cf96cf37f99
Pat LaVarre <p.lavarre@ieee.org>
2023-02-25 21:01:07 -0800

    ls.py: Ship usage: ls.py [-a] [TOP ...]  # show '.*' hidden files and dirs too
    also revive usage:  ls.py --help
    also stop compressing the Blank Lines and DocStrings out of 'def ls_tops_by_line'
    also stop inserting Comments about '-a' when showing the Code and not running it

--- bin/ls.py
+++ bin/ls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-usage: ls.py [--help] [-1 | -C | -m | -l | -lh] [--py] [TOP ...]
+usage: ls.py [--help] [-a] [-1 | -C | -m | -l | -lh] [--py] [TOP ...]
 
 show the files and dirs inside a dir
 
@@ -10,6 +10,7 @@ positional arguments:
 
 options:
   --help  show this help message and exit
+  -a      show '.*' hidden files and dirs too
   -1      show as one column of one file or dir per line
   -C      show as multiple columns
   -m      show as comma separated names
@@ -42,16 +43,22 @@ examples of write the Python for you:
   ls.py .p.py
 
   ls /dev/null/supercali  &&: exits 1 because 'Not a directory'
+  echo + exit $?
   ls.py -1 /dev/null/supercali  &&: exits 1 because NotADirectoryError
+  echo + exit $?
   ls.py -1 /dev/null/supercali --py
 
   ls -1 demos/
   ls.py -1 demos/
   ls.py -1 demos/ --py
 
-  ls -1 demos/ futures.md
-  ls.py -1 demos/ futures.md
-  ls.py -1 demos/ futures.md --py
+  ls -1 demos/ futures.md Makefile
+  ls.py -1 demos/ futures.md Makefile
+  ls.py -1 demos/ futures.md Makefile --py
+
+  ls -a1
+  ls.py -a1
+  ls.py -a1 --py
 
   python3 -c "$(ls.py --py)"
 
@@ -95,6 +102,9 @@ def main():
     # Maybe choose to do other things
 
     args = parse_ls_py_args()  # prints help & exits, when need be
+
+    main.args = args
+
     if any(list(vars(args).values())):
         run_ls_args(args)
 
@@ -131,6 +141,7 @@ def parse_ls_py_args():
     top_help = "the name of a dir or file to show"
 
     help_help = "show this help message and exit"
+    a_help = "show '.*' hidden files and dirs too"
 
     _1_help = "show as one column of one file or dir per line"
     C_help = "show as multiple columns"
@@ -149,6 +160,7 @@ def parse_ls_py_args():
     parser.add_argument("tops", metavar="TOP", nargs="*", help=top_help)
 
     parser.add_argument("--help", action="count", help=help_help)
+    parser.add_argument("-a", action="count", help=a_help)
 
     sub = parser.add_mutually_exclusive_group()
     sub.add_argument("-1", dest="one", action="count", help=_1_help)
@@ -162,6 +174,10 @@ def parse_ls_py_args():
     # Run Parser
 
     args = byo.parser_parse_args(parser)  # prints help & exits, when need be
+    if args.help:
+        parser.print_help()
+
+        sys.exit(0)
 
     return args
 
@@ -215,15 +231,14 @@ def run_ls_args(args):
 
     # Form the Code, then show it or run it
 
-    run_ls_opt_kwargs_func_tops(
-        opt, kwargs=kwargs, func=func, tops=tops, args_py=args.py
-    )
+    echo_py = echo_ls_func_tops(opt, tops=tops, func=func)
+    run_ls_opt_kwargs_func_tops(opt, kwargs=kwargs, func=func, echo_py=echo_py)
 
 
-def run_ls_opt_kwargs_func_tops(opt, kwargs, func, tops, args_py):
-    """Form the Code, then show it or run it"""
+def echo_ls_func_tops(opt, tops, func):
+    """Echo the Sh Line into a Py Comment, minus '.py' '--py' details"""
 
-    # Echo the Sh Line into a Py Comment, minus '.py' '--py' details
+    args = main.args
 
     shtops = []
     if tops:
@@ -231,18 +246,35 @@ def run_ls_opt_kwargs_func_tops(opt, kwargs, func, tops, args_py):
             shtop = byo.shlex_quote_if(top)
             shtops.append(shtop)
 
-    shline = "ls {} {}".format(opt, " ".join(shtops)).rstrip()
-    comment_py = "# {}".format(shline)
+    shline = "ls"
+    if args.a:
+        shline += " -a"
+    shline += " {} {}".format(opt, " ".join(shtops)).rstrip()
+
+    echo_py = "# {}".format(shline)
+    if func is ls_tops_by_line:
+        echo_py = '"""{}"""'.format(shline)
+
+    return echo_py
+
+
+def run_ls_opt_kwargs_func_tops(opt, kwargs, func, echo_py):
+    """Form the Code, then show it or run it"""
+
+    args = main.args
 
     # Fetch the Code
 
     func_py = byo.ast_func_to_py(func)
 
-    # Put the Sh Line Comment in place of the Func's DocString
+    if not args.a:  # stops adding 'os.curdir' and 'os.pardir' to each '.listdir()'
+        func_py = func_py.replace('[".", ".."] + ', "")
+
+    # Drop the Func's DocString
 
     lines = func_py.splitlines()
     assert lines[0].startswith('"""')
-    lines[0] = comment_py
+    lines = lines[1:]
 
     # Forward the KwArgs
 
@@ -250,20 +282,54 @@ def run_ls_opt_kwargs_func_tops(opt, kwargs, func, tops, args_py):
         kv_py = "{} = {!r}".format(k, v)
         lines[1:1] = [kv_py]
 
+    # Resolve the '-a' If's at Compile-Time
+
+    dent = 4 * " "
+
+    if args.a:
+        i = -1
+        while (i + 1) < len(lines):
+            i += 1
+            line = lines[i]
+
+            if line.strip() == 'if not item.startswith("."):':
+
+                lines[i:] = lines[i:][1:]
+                assert lines[i].startswith(dent)
+                lines[i] = lines[i][len(dent) :]
+
     # Pull in the Imports mentioned
 
     if "os." in func_py:
+        lines[1:1] = [""]
+        lines[1:1] = [""]
         lines[1:1] = ["import os"]
+
+    # Insert the DocString to Echo Bash
+
+    lines[0:0] = [""]
+    lines[0:0] = [echo_py]
+    lines[0:0] = [""]
 
     # Drop the blank lines  # deviates from Black Python Style, for tiny Programs
 
-    lines = list(_ for _ in lines if _)
+    if func is ls_tops_by_line:
+        lines[0:0] = ["#!/usr/bin/env python3"]
+    else:
+        lines = list(_ for _ in lines if _)
 
-    # Show it or run it
+    # Show the Code or run it
 
     py = "\n".join(lines)
+    run_ls_py(py)
 
-    if args_py:
+
+def run_ls_py(py):
+    """Show the Code or run it"""
+
+    args = main.args
+
+    if args.py:
         print(py)
     else:
         try:
@@ -282,7 +348,7 @@ def run_ls_opt_kwargs_func_tops(opt, kwargs, func, tops, args_py):
 def ls_here_by_line():
     """Show the Item Names of the Os GetCwd, one per Line"""
 
-    for item in sorted(os.listdir()):  # calmly empty when no Dir
+    for item in sorted([".", ".."] + os.listdir()):
         if not item.startswith("."):
             print(item)
 
@@ -290,14 +356,14 @@ def ls_here_by_line():
 def ls_file_etc_by_line(item):
     """Show the Item Name of one File, as one Line"""
 
-    _ = os.stat(item)  # raises Exception if no File
-    print(item)  # no matter if item.startswith(".")
+    _ = os.stat(item)
+    print(item)
 
 
 def ls_dir_by_line(top):
     """Show the the Item Names of one Dir, one per Line"""
 
-    for item in sorted(os.listdir(top)):  # raises Exception if File or if no Dir
+    for item in sorted([".", ".."] + os.listdir(top)):
         if not item.startswith("."):
             print(item)
 
@@ -315,13 +381,13 @@ def ls_tops_by_line(tops):
             print(top)
             sep = "\n"
 
-    for (index, topdir) in enumerate(topdirs):
+    for topdir in topdirs:
         if sep:
             print()
         print(topdir + ":")
         sep = "\n"
 
-        for item in sorted(os.listdir(topdir)):
+        for item in sorted([".", ".."] + os.listdir(topdir)):
             if not item.startswith("."):
                 print(item)
 
