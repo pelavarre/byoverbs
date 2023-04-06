d786d50506f4a1b04bd11e938135516de7f89be7
Pat LaVarre <p.lavarre@ieee.org>
2023-04-05 20:07:34 -0700

    fixup! fixup! demos/pseudotty.py: Tune up enough to track a Mouse well

--- demos/pseudotty.py
+++ demos/pseudotty.py
@@ -58,19 +58,22 @@ TOLD_BY_PACKET[b"\x07"] = "\N{Up Arrowhead}G Tty Bell"  # ⌃G
 
 TOLD_BY_PACKET[b"\x1B" b"="] = "Application Keypad (DECKPAM)"  # VT100
 TOLD_BY_PACKET[b"\x1B" b">"] = "Normal Keypad (DECKPNM)"  # VT100
+TOLD_BY_PACKET[b"\x1B" b"M"] = "GShell Esc M"  # some GShell Secret
 TOLD_BY_PACKET[b"\x1B" b"OB"] = "\N{Downwards Arrow} Mouse Down"  # ↓
 TOLD_BY_PACKET[b"\x1B" b"OC"] = "\N{Rightwards Arrow} Mouse Right"  # →
 
+TOLD_BY_PACKET[b"\x1B[" b"J"] = "Erase In Display (ED) Below"
 TOLD_BY_PACKET[b"\x1B[" b"2J"] = "Erase In Display (ED) Above Below"
-
 TOLD_BY_PACKET[b"\x1B[" b"K"] = "Erase In Line (EL) To Right"
+
+TOLD_BY_PACKET[b"\x1B[" b"34h"] = "GShell CSI 34 h"  # some GShell Secret
 
 # b"\x1B[" ... "h" = DECSET CSI ? Pm h  # DEC Private Mode Set
 TOLD_BY_PACKET[b"\x1B[" b"?1h"] = "Application Cursor Keys (DECCKM)"  # VT100
 TOLD_BY_PACKET[b"\x1B[" b"?12h"] = "Blink Cursor"
 TOLD_BY_PACKET[b"\x1B[" b"?25h"] = "Show Cursor"  # DECTCEM VT220, 1st of 2
 TOLD_BY_PACKET[b"\x1B[" b"?1000h"] = "Take Keystrokes from Mouse Press and Release"
-TOLD_BY_PACKET[b"\x1B[" b"?1034h"] = "Join Meta"  # seen without b"?1034l"
+TOLD_BY_PACKET[b"\x1B[" b"?1034h"] = "Join Meta Leak"  # seen without b"?1034l"
 TOLD_BY_PACKET[b"\x1B[" b"?1049h"] = "Alt Screen"
 TOLD_BY_PACKET[b"\x1B[" b"?2004h"] = "Alt Paste"
 
@@ -83,11 +86,19 @@ TOLD_BY_PACKET[b"\x1B[" b"?1000l"] = "Take No Keystrokes from Mouse Press and Re
 TOLD_BY_PACKET[b"\x1B[" b"?1049l"] = "Main Screen"
 TOLD_BY_PACKET[b"\x1B[" b"?2004l"] = "Main Paste"
 
-TOLD_BY_PACKET[b"\x1B[" b"23m"] = "Italic Off Blackletter Off SGR"
-TOLD_BY_PACKET[b"\x1B[" b"27m"] = "Reversed Off SGR"  # Positive On SGR
-TOLD_BY_PACKET[b"\x1B[" b"29m"] = "Strikethrough Off SGR"  # Crossed Out Off
-TOLD_BY_PACKET[b"\x1B[" b"94m"] = "Bright Blue SGR"
+TOLD_BY_PACKET[b"\x1B[" b"00m"] = "Exit SGR 00 Variation"
+TOLD_BY_PACKET[b"\x1B[" b"0m"] = "Exit SGR 0 Variation"
 TOLD_BY_PACKET[b"\x1B[" b"m"] = "Exit SGR"
+
+TOLD_BY_PACKET[b"\x1B[" b"1;34m"] = "Bold SGR with Blue SGR"
+TOLD_BY_PACKET[b"\x1B[" b"1m"] = "Bold SGR"
+TOLD_BY_PACKET[b"\x1B[" b"3m"] = "Italic SGR"
+TOLD_BY_PACKET[b"\x1B[" b"24m"] = "Underlined Exit SGR"
+TOLD_BY_PACKET[b"\x1B[" b"34m"] = "Blue SGR"
+TOLD_BY_PACKET[b"\x1B[" b"23m"] = "Italic Exit Blackletter Exit SGR"
+TOLD_BY_PACKET[b"\x1B[" b"27m"] = "Reversed Exit SGR"  # Positive On SGR
+TOLD_BY_PACKET[b"\x1B[" b"29m"] = "Strikethrough Exit SGR"  # Crossed Out Exit
+TOLD_BY_PACKET[b"\x1B[" b"94m"] = "Bright Blue SGR"
 
 TOLD_BY_PACKET[b"\x1B[" b">4;2m"] = "Enter XTQMODKEYS"
 TOLD_BY_PACKET[b"\x1B[" b">4;m"] = "Exit XTQMODKEYS"
@@ -101,12 +112,21 @@ TOLD_BY_PACKET[b"\x1B[" b"23;0;0t"] = "Exit XTSMTITLE 23 0 0"
 TOLD_BY_PACKET[b"\x1B[" b"23;1t"] = "Pop Icon Title"
 TOLD_BY_PACKET[b"\x1B[" b"23;2t"] = "Pop Window Title"
 
+TOLD_BY_PACKET[b"\x1B[" b"200~"] = "Enter Bracketed Paste"  # for Mode "rb"
+TOLD_BY_PACKET[b"\x1B[" b"201~"] = "Exit Bracketed Paste"  # for Mode "rb"
+
 TOLD_BY_PACKET_KEYS_MINUS = list(TOLD_BY_PACKET.keys())
 
 TOLD_BY_PACKET[b"\x1B"] = "\N{Broken Circle With Northwest Arrow} Esc"  # ⎋
 
-# begun with my 2023-04 sample of Monterey macOS 12.6.3
-# next Ubuntu 2020|2022 => \x07 \e[23m \e[22;0;0t \e[23;0;0t
+#
+# i began with my 2023-04 sample of Monterey macOS 12.6.3
+#
+# next Ubuntu 2020|2022 =>  \x07  \e[23m \e[22;0;0t \e[23;0;0t
+#
+# next G Shell =>  \eM \e\ \e] \ek  \e[34h \e[J  \[200~ \[201~
+# also G Shell =>  \e[00m \e[0m \e[1;34m \e1m \e3m \e24m \e34m
+#
 
 
 def main():
@@ -215,11 +235,18 @@ def pty_bytes_split_once(bytes_, mode):  # noqa
 
     assert mode in "rb wb".split(), (mode,)
 
+    # Declare \e[ CSI
+
     csi_digits = r"0-9;>\?"
-    csi_pattern = "\x1b\\[[{}]*[^{}]".format(csi_digits, csi_digits).encode()
-    csi_pattern = "\x1b\\[M...|".encode() + csi_pattern
+    csi_pattern = "\x1B\\[[{}]*[^{}]".format(csi_digits, csi_digits).encode()
+    csi_pattern = "\x1B\\[M...|".encode() + csi_pattern
 
     assert csi_pattern == CSI_PATTERN
+
+    # Declare \e] OSC and \ek Alt OSC
+
+    osc_pattern = "\x1B][^\x07\x1B]*(\x07|\x1B\\\\)".encode()
+    alt_osc_pattern = "\x1Bk[^\x1B]*\x1B\\\\".encode()
 
     # Split out 1 Escape Sequence begun by the Control Sequence Introducer (CSI)
 
@@ -229,7 +256,25 @@ def pty_bytes_split_once(bytes_, mode):  # noqa
             packet = m.group(0)
         else:
             print("CSI Pattern", csi_pattern.decode().replace("\x1B", r"\e"))  # Stderr?
-            assert False, (packet,)
+            assert False, (bytes_[:50],)
+            packet = bytes_[:2]
+
+    elif bytes_[:2] == b"\x1B]":
+        m = re.match(osc_pattern, string=bytes_)
+        if m:
+            packet = m.group(0)
+        else:
+            print("OSC Pattern", osc_pattern.decode().replace("\x1B", r"\e"))
+            assert False, (bytes_[:50],)
+            packet = bytes_[:2]
+
+    elif bytes_[:2] == b"\x1Bk":
+        m = re.match(alt_osc_pattern, string=bytes_)
+        if m:
+            packet = m.group(0)
+        else:
+            print("Alt OSC Pattern", alt_osc_pattern.decode().replace("\x1B", r"\e"))
+            assert False, (bytes_[:50],)
             packet = bytes_[:2]
 
     # Split out the widest Defined Packet
@@ -346,6 +391,12 @@ def pty_packet_told(packet):
 
     if packet in TOLD_BY_PACKET.keys():
         told = TOLD_BY_PACKET[packet]
+
+    elif packet.startswith(b"\x1B]"):  # Operating System Command (OSC)
+        told = "Operating-System-Command (OSC)"
+
+    elif packet.startswith(b"\x1Bk"):  # Alt OSC
+        told = "GShell Esc k"  # some GShell Secret
 
     elif packet.startswith(b"\x1B["):  # Control Sequence Introducer (CSI)
         if packet.startswith(b"\x1B[M") and (len(packet) == 6):
