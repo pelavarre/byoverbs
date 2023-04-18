0f5c94f4733d95061eef5e3b49ff3540cd22b6b6
Pat LaVarre <p.lavarre@ieee.org>
2023-04-17 17:16:52 -0700

    demos/diffable.py = Copy input to output, but make it more diffable
    first solve my own Html tonight

---
+++ demos/diffable.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+
+r"""
+usage: diffable.py [-h]
+
+copy input to output, but make it more diffable
+
+options:
+  -h, --help  show this help message and exit
+
+examples:
+  diff -brpu <(demos/diffable.py -- <a.html) <(demos/diffable.py -- <b.html)
+  diff -brpu <(demos/diffable.py -- <a.html |sort) <(demos/diffable.py -- <b.html |sort)
+"""
+
+# code reviewed by people, and by Black and Flake8
+
+import __main__
+import argparse
+import difflib
+import os
+import sys
+import textwrap
+
+
+#
+# Run from the Sh command line
+#
+
+
+def main():
+    """Run from the Sh command line"""
+
+    parser = compile_argdoc()
+    parser_parse_args(parser)  # prints help and exits zero, when asked
+
+    ilines = sys.stdin.readlines()
+    for iline in ilines:
+        oline = ""
+        for before, ch, after in zip("\n" + iline, iline, iline[1:] + "\n"):
+            if ch == "<":
+                if before != "\n":
+                    oline += "\n"
+
+            oline += ch
+
+            if ch == ">":
+                if after != "\n":
+                    oline += "\n"
+
+        print(oline)
+
+
+#
+# Add some Def's to Import ArgParse
+#
+
+
+def compile_argdoc(drop_help=None):
+    """Form an ArgumentParser from the Main Doc, without Positional Args and Options"""
+
+    argdoc = __main__.__doc__
+
+    # Compile much of the Arg Doc to Args of 'argparse.ArgumentParser'
+
+    doc_lines = argdoc.strip().splitlines()
+    prog = doc_lines[0].split()[1]  # first word of first line
+
+    doc_firstlines = list(_ for _ in doc_lines if _ and (_ == _.lstrip()))
+    description = doc_firstlines[1]  # first line of second paragraph
+
+    add_help = not drop_help
+
+    # Pick the Epilog out of the Doc
+
+    epilog = None
+    for index, line in enumerate(doc_lines):
+        strip = line.rstrip()
+
+        skip = False
+        skip = skip or not strip
+        skip = skip or strip.startswith(" ")
+        skip = skip or strip.startswith("usage")
+        skip = skip or strip.startswith(description.rstrip())
+        skip = skip or strip.startswith("positional arguments")
+        skip = skip or strip.startswith("options")
+        if skip:
+            continue
+
+        epilog = "\n".join(doc_lines[index:])
+
+        break
+
+    # Form an ArgumentParser, without Positional Args and Options
+
+    parser = argparse.ArgumentParser(
+        prog=prog,
+        description=description,
+        add_help=add_help,
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=epilog,
+    )
+
+    return parser
+
+
+def parser_parse_args(parser):
+    """Parse the Sh Args, even when no Sh Args coded as the one Sh Arg '--'"""
+
+    sys_exit_if_argdoc_ne(parser)  # prints diff and exits nonzero, when Arg Doc wrong
+
+    sh_args = sys.argv[1:]
+    if sh_args == ["--"]:  # needed by ArgParse when no Positional Args
+        sh_args = ""
+
+    args = parser.parse_args(sh_args)  # prints helps and exits, else returns args
+
+    sys_exit_if_testdoc(parser.epilog)  # prints examples & exits if no args
+
+    return args
+
+
+def sys_exit_if_argdoc_ne(parser):
+    """Print Diff and exit nonzero, unless Arg Doc equals Parser Format_Help"""
+
+    # Fetch the Main Doc, and note where from
+
+    main_doc = __main__.__doc__.strip()
+    main_filename = os.path.split(__file__)[-1]
+    got_filename = "./{} --help".format(main_filename)
+
+    # Fetch the Parser Doc from a fitting virtual Terminal
+    # Fetch from a Black Terminal of 89 columns, not current Terminal width
+    # Fetch from later Python of "options:", not earlier Python of "optional arguments:"
+
+    with_columns = os.getenv("COLUMNS")
+    os.environ["COLUMNS"] = str(89)
+    try:
+        parser_doc = parser.format_help()
+
+    finally:
+        if with_columns is None:
+            os.environ.pop("COLUMNS")
+        else:
+            os.environ["COLUMNS"] = with_columns
+
+    parser_doc = parser_doc.replace("optional arguments:", "options:")
+
+    parser_filename = "ArgumentParser(...)"
+    want_filename = parser_filename
+
+    # Print the Diff to Parser Doc from Main Doc and exit, if Diff exists
+
+    got_doc = main_doc
+    want_doc = parser_doc
+
+    diffs = list(
+        difflib.unified_diff(
+            a=got_doc.splitlines(),
+            b=want_doc.splitlines(),
+            fromfile=got_filename,
+            tofile=want_filename,
+            lineterm="",  # else the '---' '+++' '@@' Diff Control Lines end with '\n'
+        )
+    )
+
+    if diffs:
+        print("\n".join(diffs))
+
+        sys.exit(2)  # trust caller to log SystemExit exceptions well
+
+    # https://github.com/python/cpython/issues/53903  <= options: / optional arguments:
+    # https://bugs.python.org/issue38438  <= usage: [WORD ... ] / [WORD [WORD ...]]
+
+    # todo: tag the 'a=' 'b=' such that 'git apply -v' fixes the failure
+
+
+#
+# Add some Def's to Import Sys
+#
+
+
+def sys_exit_if_testdoc(epilog):
+    """Print examples and exit, if no Sh Args"""
+
+    if sys.argv[1:]:
+        return
+
+    lines = epilog.splitlines()
+    indices = list(_ for _ in range(len(lines)) if lines[_])
+    indices = list(_ for _ in range(len(lines)) if not lines[_].startswith(" "))
+    testdoc = "\n".join(lines[indices[-1] + 1 :])
+    testdoc = textwrap.dedent(testdoc)
+
+    print()
+    print(testdoc)
+    print()
+
+    sys.exit(0)
+
+
+#
+# Sketch future work
+#
+
+
+# todo: less wrong answers
+
+
+#
+# Run from the Sh command line, when not imported
+#
+
+
+if __name__ == "__main__":
+    main()
+
+
+# posted into:  https://github.com/pelavarre/byoverbs/blob/main/demos/diffable.py
+# copied from:  git clone https://github.com/pelavarre/byoverbs.git
