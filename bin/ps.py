e7c9cb608dbdd651217a2a7c3eacf05beabf1c20
Pat LaVarre <p.lavarre@ieee.org>
2023-09-21 16:18:26 -0700

    bin/ps.py: Start remembering all 3 of:  ps aux, ps auwwx, ps a -wo pid,lstart,cmd

---
+++ bin/ps.py
@@ -0,0 +1,7 @@
+# ps aux
+# ps auwwx
+# ps a -wo pid,lstart,cmd  # suggested by StackOverflow
+
+
+# posted into:  https://github.com/pelavarre/byoverbs/blob/main/bin/uptime.py
+# copied from:  git clone https://github.com/pelavarre/byoverbs.git
