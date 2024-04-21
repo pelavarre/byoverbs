9d7b7b4033350048076949253f9661ef7c635e9b
Pat LaVarre <p.lavarre@ieee.org>
2024-04-20 19:50:21 -0700

    bin/pq3.py: Move the last 'bin/pq.py' experiment out of the way

--- bin/pq3.py
+++ bin/pq3.py
@@ -505,5 +505,5 @@ if __name__ == "__main__":
     main()
 
 
-# posted into:  https://github.com/pelavarre/byoverbs/blob/main/bin/pq.py
+# posted into:  https://github.com/pelavarre/byoverbs/blob/main/bin/pq3.py
 # copied from:  git clone https://github.com/pelavarre/byoverbs.git
