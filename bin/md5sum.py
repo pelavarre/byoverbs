41edaec66fc33e66342b8a8b74a175b9e296839f
Pat LaVarre <p.lavarre@ieee.org>
2024-04-23 14:36:18 -0700

    qla: Take 0 or more non-option Args as Author Names

---
+++ bin/md5sum.py
@@ -0,0 +1,5 @@
+# git show |awk '/^$/{f=1} f{print}' |md5sum
+
+
+# posted into:  https://github.com/pelavarre/byoverbs/blob/main/bin/md5sum.py
+# copied from:  git clone https://github.com/pelavarre/byoverbs.git
