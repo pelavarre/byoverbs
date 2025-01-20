58df68012f0b9b1169d8af3a9de580e0377cce38
Pat LaVarre <p.lavarre@ieee.org>
2025-01-20 12:23:09 -0800

    hexdump.py,od.py: Celebrate ways of saying 'hexdump -C'

--- bin/hexdump.py
+++ bin/hexdump.py
@@ -8,5 +8,5 @@
 # $
 
 
-# posted as:  https://github.com/pelavarre/byoverbs/blob/main/bin/od.py
+# posted as:  https://github.com/pelavarre/byoverbs/blob/main/bin/hexdump.py
 # copied from:  git clone https://github.com/pelavarre/byoverbs.git
