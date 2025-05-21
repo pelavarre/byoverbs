683907c33c33f83911fba6eb4f2c1f5e4fc6929a
Pat LaVarre <p.lavarre@ieee.org>
2025-05-20 19:17:38 -0700

    flake8.bash: Bend PyPi Flake8 to fit with PyPi Black for typing.Protocol's

--- bin/flake8.bash
+++ bin/flake8.bash
@@ -4,12 +4,13 @@ set -e
 
 # shellcheck disable=SC2088  # Tilde does not expand in quotes, but $HOME does
 echo + '~/.pyvenvs/flake8/bin/flake8' \
-    --max-line-length=999 --max-complexity 10 --ignore=E203,W503 "$@"
+    --max-line-length=999 --max-complexity 10 --ignore=E203,E704,W503 "$@"
 
 ~/.pyvenvs/flake8/bin/flake8 \
-    --max-line-length=999 --max-complexity 10 --ignore=E203,W503 "$@"
+    --max-line-length=999 --max-complexity 10 --ignore=E203,E704,W503 "$@"
 
 # --max-line-length=999  # Black max line lengths over Flake8 max line lengths
 # --max-complexity 10  # limit how much McCabe Cyclomatic Complexity we accept
 # --ignore=E203  # Black '[ : ]' rules over E203 whitespace before ':'
+# --ignore=E704  # Black of typing.Protocol rules over E704 multiple statements on one line (def)
 # --ignore=W503  # 2017 Pep 8 and Black over W503 line break before bin op
