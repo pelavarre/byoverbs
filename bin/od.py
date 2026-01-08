bc80a9fb33d0be4053dee9ef57c9a607e6c4c527
Pat LaVarre <p.lavarre@ieee.org>
2026-01-08 10:57:09 -0800

    od.py: Clarify our example encodings of the idea of Cls 'clear' 'tput clear'

--- bin/od.py
+++ bin/od.py
@@ -1,11 +1,22 @@
-# hexdump -C
 # od -A x -t x1 -v
+# hexdump -C
+
+
+# macOS
+# % tput clear |hexdump -C |column -t
+# 00000000  1b  5b  48  1b  5b  32  4a  |.[H.[2J|
+# 00000007
+# %
 
 # Linux
-# $ clear |od -A x -t x1z -v
-# 000000 1b 5b 48 1b 5b 32 4a 1b 5b 33 4a                 >.[H.[2J.[3J<
+# $ clear |od -A x -t x1z -v |column -t
+# 000000  1b  5b  48  1b  5b  32  4a  1b  5b  33  4a  >.[H.[2J.[3J<
 # 00000b
 # $
+
+# printf '\033[H''\033[2J'  # row-column-leap screen-erase
+# printf '\033[3J''\033[H''\033[2J'  # scroll-erase but keep a scrolled copy of screen
+# printf '\033[H''\033[2J''\033[3J'  # really do erase it all, like as ⌘K does
 
 
 # posted as:  https://github.com/pelavarre/byoverbs/blob/main/bin/od.py
