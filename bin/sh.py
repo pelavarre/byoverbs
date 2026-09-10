79e19394a23160e036c39e1a777ed6abc721fd85
Pat LaVarre <p.lavarre@ieee.org>
2026-09-09 18:26:17 -0700

    Stop shipping into ~/bin/sh/__main__.py

--- bin/sh.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sh.__main__
-
-sh.__main__.main()
-
-
-# posted as:  https://github.com/pelavarre/byoverbs/blob/main/bin/sh.py
-# copied from:  git clone https://github.com/pelavarre/byoverbs.git
--- bin/sh/__main__.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-def main():
-    assert not sys.argv[1:], (sys.argv[0], sys.argv[1:])
-
-    print("bash --noprofile --norc")
-    print("exit 3")
-    print("zsh -f")
