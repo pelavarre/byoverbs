fba458ccce8a7ce442fac66d6cd23f6310e4c1dc
Pat LaVarre <p.lavarre@ieee.org>
2026-09-10 21:13:54 -0700

    bin/flake8.bash: Adopt --extend-ignore over the Flake8 --ignore that means --ignore-none-but

--- bin/flake8.bash
+++ bin/flake8.bash
@@ -4,10 +4,10 @@ set -e
 
 # shellcheck disable=SC2088  # Tilde does not expand in quotes, but $HOME does
 echo + '~/.pyvenvs/flake8/bin/flake8' \
-    --max-line-length=999 --max-complexity 15 --ignore=E203,E704,W503 "$@"
+    --max-line-length=999 --max-complexity 15 --extend-ignore=E203,E704,W503 "$@"
 
 ~/.pyvenvs/flake8/bin/flake8 \
-    --max-line-length=999 --max-complexity 15 --ignore=E203,E704,W503 "$@" 2>&1 \
+    --max-line-length=999 --max-complexity 15 --extend-ignore=E203,E704,W503 "$@" 2>&1 \
 	|grep -v '^/[^:]*flake8_import_order/styles.py:' \
 	|grep -v '  from pkg_resources import iter_entry_points' \
 	|cat -
@@ -15,6 +15,9 @@ echo + '~/.pyvenvs/flake8/bin/flake8' \
 
 # --max-line-length=999  # Black max line lengths over Flake8 max line lengths
 # --max-complexity 15  # limit how much McCabe Cyclomatic Complexity we accept
-# --ignore=E203  # Black '[ : ]' rules over E203 whitespace before ':'
-# --ignore=E704  # Black of typing.Protocol rules over E704 multiple statements on one line (def)
-# --ignore=W503  # 2017 Pep 8 and Black over W503 line break before bin op
+# --extend-ignore  # adds to default ignores (--ignore replaces)
+# --extend-ignore=E203  # Black '[ : ]' rules over E203 whitespace before ':'
+# --extend-ignore=E704  # Black of typing.Protocol rules over E704 multi statements on one line (def)
+# --extend-ignore=W503  # 2017 Pep 8 and Black over W503 line break before bin op
+
+# --extend-ignore=E501  # Line too long (... > ... characters) (E501)
